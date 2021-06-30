e24700354f6588e61cd9a54803e84843d02187f8
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-06-30 11:11:19 -0500

    First Tests With StepsVerifier

---
+++ src/test/java/com/example/starterwebflux/StarterWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starterwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
